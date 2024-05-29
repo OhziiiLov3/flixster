096710a086153e4672b2ede78688df198b1102a4
ohziiilov3 <klbaskerville0520@gmail.com>
2024-05-29 14:34:55 -0700

    chore: add header, footer with styling chore: add searchBar and dropdown placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import './App.css'
+import Footer from './components/Footer';
 import Header from './components/Header'
 
 
@@ -7,6 +8,12 @@ const App = () => {
   return (
     <div className="App">
      <Header/>
+
+     <div className="App-header">
+       <div>Search Bar</div>
+       <div>Dropdown Filter</div>
+     </div>
+     <Footer/>
     </div>
   );
  
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
-
+import '../styles/Header.css';
 
 const Header = () => {
-  return <div className="App-header">Flixster</div>;
+  return (
+    <div className="header" >
+      <h1>🎥 Flixster 🎬</h1>
+    </div>
+  );
 }
 
 export default Header
